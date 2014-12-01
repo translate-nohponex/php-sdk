8994e024c81eba507d01b3186285bab5f1e7334c
NohponeX <NohponeX@gmail.com>
2014-12-01 15:54:39 +0200

    Add detailed comments

--- test.php
+++ test.php
@@ -1,31 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
   <head>
     <meta charset="utf-8">
+    <title>Demo</title>
   </head>
 <body>
     <pre>
-<?php
+    <?php
+    //Display all errors
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-require 'src/translate.php';
+    //Include Translate SDK class
+    require 'src/translate.php';
 
     //Initialize API 
     $translate = new Translate\Translate( '59f1cbb3bee39038d74e9d043daf016a' );
 
     try{
-      
+        //Print all thranslations for 'gr' language
         print_r ( $translate->fetch( '1', 'gr' ) );
 
     }catch( Translate\TranslateAPIException $e ){
-
         print( $e->getMessage( ) );
         echo '</pre>';
         die();
     }
-?>
-</pre>
+    ?>
+    </pre>
 </body>
 </html>
